57a459fc4bb699ebae579d9743e3174e528dc0d6
Daniel Brazel <dbrazel4@gmail.com>
2018-05-14 13:06:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(place){
+  if (place > 42) {
+    return place - 42
+  } else {
+    return 42 - place
+  }
+}
+
+function distanceFromHqInFeet(place){
+  return (distanceFromHqInBlocks(place) * 264)
+}
+
+function distanceTravelledInFeet(start, end) {
+  if (start > end){
+    return (start - end) * 264
+  } else {
+    return (end - start) * 264
+  }
+}
+
+function calculatesFarePrice(start, end){
+  let distance = distanceTravelledInFeet(start, end)
+  if (distance <= 400){
+    return 0
+  } else if (distance > 400 && distance <= 2000){
+    return (distance - 400) * 0.02
+  } else if (distance > 2000 && distance <= 2500){
+    return 25
+  } else {
+    return 'cannot travel that far'
+  }
+}
