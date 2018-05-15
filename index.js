f49b14f3a9bb388fa67a9743aee0a1b019e5d346
Ahamed <ahamedali95@gmail.com>
2018-05-14 22:50:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  return Math.abs(block - 42);
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+  return Math.abs(distanceFromHqInFeet(distanceFromHqInBlocks(endingBlock)) - distanceFromHqInFeet(distanceFromHqInBlocks(startingBlock)));
+}
+
+function calculatesFarePrice(startingBlock, endingBlock) {
+  const distance = distanceTravelledInFeet(startingBlock, endingBlock);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
